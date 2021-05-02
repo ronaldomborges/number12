ac518d6181671158f79112b388e4980f60fd569a
ronaldomborges <65875791+ronaldomborges@users.noreply.github.com>
2021-05-02 11:10:45 -0300

    1

---
+++ script.js
@@ -0,0 +1,6 @@
+function lighton() {
+    document.getElementById('myimage').src = "imagens/bulbon.gif";
+  }
+  function lightoff() {
+    document.getElementById('myimage').src = "imagens/off.gif";
+  }
